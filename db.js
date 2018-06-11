eb937214c30303a6edf3097d68d392e19883e8bd
Lucio Afonso <36516559+UnderGrounder96@users.noreply.github.com>
2018-06-11 09:57:22 +0200

    Add files via upload
    Not fully tested! @.@

--- db.js
+++ db.js
@@ -37,9 +37,13 @@ db.sequelize = sequelize;
 //Models/tables
 db.author = require('./models/author')(sequelize, Sequelize);
 db.book = require('./models/book')(sequelize, Sequelize);
+db.book_author_category = require('./models/book_author_category')(sequelize, Sequelize);
+db.book_info = require('./models/book_info')(sequelize, Sequelize);
 db.book_marks = require('./models/book_marks')(sequelize, Sequelize);
 db.book_series = require('./models/book_series')(sequelize, Sequelize);
+db.book_series_view = require('./models/book_series_view')(sequelize, Sequelize);
 db.book_status = require('./models/book_status')(sequelize, Sequelize);
+db.book_totalmarks_avgmark = require('./models/book_totalmarks_avgmark')(sequelize, Sequelize);
 db.category = require('./models/category')(sequelize, Sequelize);
 db.mark = require('./models/mark')(sequelize, Sequelize);
 db.series = require('./models/series')(sequelize, Sequelize);
@@ -48,25 +52,108 @@ db.user = require('./models/user')(sequelize, Sequelize);
 
 //Relations
 db.author.hasMany(db.book, { foreignKey: 'idauthors' }); // field 'idauthors' in Book model reffers to Author model
+db.author.hasMany(db.book_author_category, { foreignKey: 'idauthors' });
+db.author.hasMany(db.book_author_category, { foreignKey: 'name' });
 db.author.hasMany(db.series, { foreignKey: 'idauthors' });
 db.book.belongsTo(db.author, { foreignKey: 'idauthors' }); //  'idauthors' attribute added to Book model
+db.book_author_category.belongsTo(db.author, { foreignKey: 'idauthors' });
+db.book_author_category.belongsTo(db.author, { foreignKey: 'name' });
 db.series.belongsTo(db.author, { foreignKey: 'idauthors' });
 
+db.book.hasMany(db.book_author_category, { foreignKey: 'idbooks' });
+db.book.hasMany(db.book_author_category, { foreignKey: 'title' });
+db.book.hasMany(db.book_author_category, { foreignKey: 'publish_date' });
+db.book.hasMany(db.book_author_category, { foreignKey: 'ID_NUMBER' });
+db.book.hasMany(db.book_author_category, { foreignKey: 'publisher' });
+db.book.hasMany(db.book_author_category, { foreignKey: 'synopsis' });
+db.book.hasMany(db.book_author_category, { foreignKey: 'image' });
+db.book.hasMany(db.book_author_category, { foreignKey: 'text' });
+db.book.hasMany(db.book_author_category, { foreignKey: 'original_title' });
+db.book.hasMany(db.book_author_category, { foreignKey: 'language' });
 db.book.hasMany(db.book_marks, { foreignKey: 'idbooks' });
 db.book.hasMany(db.book_series, { foreignKey: 'idbooks' });
+db.book.hasMany(db.book_series_view, { foreignKey: 'idbooks' });
 db.book.hasMany(db.book_status, { foreignKey: 'idbooks' });
+db.book.hasMany(db.book_totalmarks_avgmark, { foreignKey: 'idbooks' });
+db.book_author_category.belongsTo(db.book, { foreignKey: 'idbooks' });
+db.book_author_category.belongsTo(db.book, { foreignKey: 'title' });
+db.book_author_category.belongsTo(db.book, { foreignKey: 'publish_date' });
+db.book_author_category.belongsTo(db.book, { foreignKey: 'ID_NUMBER' });
+db.book_author_category.belongsTo(db.book, { foreignKey: 'publisher' });
+db.book_author_category.belongsTo(db.book, { foreignKey: 'synopsis' });
+db.book_author_category.belongsTo(db.book, { foreignKey: 'text' });
+db.book_author_category.belongsTo(db.book, { foreignKey: 'original_title' });
+db.book_author_category.belongsTo(db.book, { foreignKey: 'language' });
 db.book_marks.belongsTo(db.book, { foreignKey: 'idbooks' });
 db.book_series.belongsTo(db.book, { foreignKey: 'idbooks' });
+db.book_series_view.belongsTo(db.book, { foreignKey: 'idbooks' });
 db.book_status.belongsTo(db.book, { foreignKey: 'idbooks' });
+db.book_totalmarks_avgmark.belongsTo(db.book, { foreignKey: 'idbooks' });
+
+
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'idbooks' });
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'title' });
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'publish_date' });
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'ID_NUMBER' });
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'publisher' });
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'synopsis' });
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'image' });
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'text' });
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'original_title' });
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'language' });
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'idauthors' });
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'name' });
+db.book_author_category.hasMany(db.book_info, { foreignKey: 'idcategories' });
+db.book_info.belongsTo(db.book_author_category, { foreignKey: 'idbooks' });
+db.book_info.belongsTo(db.book_author_category, { foreignKey: 'title' });
+db.book_info.belongsTo(db.book_author_category, { foreignKey: 'publish_date' });
+db.book_info.belongsTo(db.book_author_category, { foreignKey: 'ID_NUMBER' });
+db.book_info.belongsTo(db.book_author_category, { foreignKey: 'publisher' });
+db.book_info.belongsTo(db.book_author_category, { foreignKey: 'synopsis' });
+db.book_info.belongsTo(db.book_author_category, { foreignKey: 'text' });
+db.book_info.belongsTo(db.book_author_category, { foreignKey: 'original_title' });
+db.book_info.belongsTo(db.book_author_category, { foreignKey: 'language' });
+db.book_info.belongsTo(db.book_author_category, { foreignKey: 'idauthors' });
+db.book_info.belongsTo(db.book_author_category, { foreignKey: 'name' });
+db.book_info.belongsTo(db.book_author_category, { foreignKey: 'idcategories' });
+
+
+db.book_series.hasMany(db.book_series_view, { foreignKey: 'vol_in_series' });
+db.book_series_view.belongsTo(db.book_series, { foreignKey: 'vol_in_series' });
+
+
+db.book_series_view.hasMany(db.book_info, { foreignKey: 'series_name' });
+db.book_series_view.hasMany(db.book_info, { foreignKey: 'idseries' });
+db.book_series_view.hasMany(db.book_info, { foreignKey: 'how_many_in_series' });
+db.book_series_view.hasMany(db.book_info, { foreignKey: 'vol_in_series' });
+db.book_info.belongsTo(db.book_series, { foreignKey: 'series_name' });
+db.book_info.belongsTo(db.book_series, { foreignKey: 'idseries' });
+db.book_info.belongsTo(db.book_series, { foreignKey: 'how_many_in_series' });
+db.book_info.belongsTo(db.book_series, { foreignKey: 'vol_in_series' });
+
+
+db.book_totalmarks_avgmark.hasMany(db.book_info, { foreignKey: 'idbooks' });
+db.book_info.belongsTo(db.book_totalmarks_avgmark, { foreignKey: 'value' });
+
 
 db.category.hasMany(db.book, { foreignKey: 'idcategories' });
+db.category.hasMany(db.book_author_category, { foreignKey: 'idcategories' });
 db.book.belongsTo(db.category, { foreignKey: 'idcategories' });
+db.book_author_category.belongsTo(db.category, { foreignKey: 'idcategories' });
 
 db.mark.hasMany(db.book_marks, { foreignKey: 'idmarks' });
+db.mark.hasMany(db.book_totalmarks_avgmark, { foreignKey: 'value' });
 db.book_marks.belongsTo(db.mark, { foreignKey: 'idmarks' });
+db.book_totalmarks_avgmark.belongsTo(db.mark, { foreignKey: 'value' });
 
 db.series.hasMany(db.book_series, { foreignKey: 'idseries' });
+db.series.hasMany(db.book_series_view, { foreignKey: 'idseries' });
+db.series.hasMany(db.book_series_view, { foreignKey: 'series_name' });
+db.series.hasMany(db.book_series_view, { foreignKey: 'how_many_in_series' });
 db.book_series.belongsTo(db.series, { foreignKey: 'idseries' });
+db.book_series_view.belongsTo(db.series, { foreignKey: 'idseries' });
+db.book_series_view.belongsTo(db.series, { foreignKey: 'series_name' });
+db.book_series_view.belongsTo(db.series, { foreignKey: 'how_many_in_series' });
 
 db.status.hasMany(db.book_status, { foreignKey: 'idstatus' });
 db.book_status.belongsTo(db.status, { foreignKey: 'idstatus' });
