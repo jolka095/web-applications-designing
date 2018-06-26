7d762c35e7c1242f3f92fa20cf6e45f5a25c874a
Lucio Afonso <36516559+UnderGrounder96@users.noreply.github.com>
2018-06-26 15:30:48 +0200

    Add files via upload

--- routes/library.js
+++ routes/library.js
@@ -4,79 +4,36 @@ const auth = require('../authentication/middleware');
 const router = express.Router();
 const db = require('../db');
 const helper = require('../public/js/helper');
-const Op = Sequelize.Op;
-
 
 router.get('/', auth(), function (req, res) {
+    const idUser = req.user ? req.user.idUser : null;
 
-    // checking inf user has any books in his library (status: "to_read", "reading" or "done")
     db.statuses.findAll({
-        where: {
-            idUser: req.user.idUser
-        }
+        where: {idUser: req.user.idUser},
+        include: [{ model: db.book, include: [db.author]}]
     })
-    .then(statuses => {
+        .then(result=>{
+            const statContainer = [];
 
-        let idBookArr = []
-        statuses.forEach(status => {
-            idBookArr.push(status.idBook)
-        });
-        // console.log(JSON.stringify(idBookArr, null, 2));
-        return idBookArr
-    })
-    .then(idsArr => {
-        // finding all books from user library
-        db.book.findAll({
-            where: {
-                idBook: {
-                    [Op.in]: idsArr
-                }
-            },
-            include: [
-                db.author,
-                db.mark,
-                db.statuses,
-                {
-                    model: db.bookSeries,
-                    include: [db.series]
-                }
-            ]
-        })
-            .then(result => {
+            result.forEach(stat => {
 
+                statContainer.push({
+                    status: stat, //? helper.getBookStatusForUser(stat, idUser) : null,
+                    details: stat.book,
+                    author: stat.book.author
+                });
+            });
 
-                if (typeof result !== 'undefined' && result !== null) {
-                    // console.log(JSON.stringify(result, null, 2));
-                    const booksContainer = []
-
-                    result.forEach(book => {
-                        if (typeof book !== 'undefined' && book !== null) {                        
-                            booksContainer.push({
-                                details:            book,
-                                author:             book.author,
-                                marksArr:           ((typeof book.marks !== 'undefined' && book.marks !== null) ? book.marks : null),
-                                avgMark:            helper.getAverageMarkForBook(book.marks),
-                                userMark:           helper.getUserMarkForBook(book.marks, req.user.idUser),
-                                status:             ((typeof book.statuses !== 'undefined' && book.statuses !== null) ? helper.getBookStatusForUser(book.statuses, req.user.idUser) : null),
-                                volNumberInSeries:  ((typeof book.bookseries !== 'undefined' && book.bookseries !== null) ? ((book.bookseries.length > 0) ? book.bookseries[0].booksNumber : null) : null),
-                                series:             ((typeof book.bookseries !== 'undefined' && book.bookseries !== null) ? ((book.bookseries.length > 0) ? book.bookseries[0].series : null) : null)
-                            })
-                        } else {
-                            console.log("Book undefined or null")
-                        }
-                    })
-
-                    res.render('library', { booksArr: booksContainer, result, user: req.user })
-                } else {
-                    res.send("Nie znaleziono ksiażek takiego usera w bazie");
-                }
-            })
+            //console.log('result: '+JSON.stringify(statContainer[0].details, null, 2));
+            //console.log('stat: '+JSON.stringify(statContainer[0].author, null, 2));
+            res.render('library', { booksArr: statContainer, user: req.user });
         })
         .catch(error => {
             console.log(error);
             res.render('library', { booksArr: null, user: req.user });
             // res.status(400).send(error);
         });
+
 
 });
 
